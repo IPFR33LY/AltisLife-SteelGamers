aac325150827273c2f4b87ef7ab30c7dbb70627a
Shems <Shems-SH@users.noreply.github.com>
2015-05-09 15:45:42 +0200

    Update Functions.h

--- Mission/Functions.h
+++ Mission/Functions.h
@@ -44,19 +44,19 @@ class Life_Client_Core
 	class Admin
 	{
 		file = "core\admin";
-        class admininfo {};
-        class adminid {};
-        class admingetID {};
-        class adminMenu {};
-        class adminQuery {};
-        class adminSpectate {};
-        class adminTeleport {};
-        class adminTpHere {};
-        class adminDebugCon {};
-        class adminCompensate {};
-        class adminGodMode {};
-        class adminFreeze {};
-        class adminMarkers {};
+	        class admininfo {};
+	        class adminid {};
+	        class admingetID {};
+	        class adminMenu {};
+	        class adminQuery {};
+	        class adminSpectate {};
+	        class adminTeleport {};
+	        class adminTpHere {};
+	        class adminDebugCon {};
+	        class adminCompensate {};
+	        class adminGodMode {};
+	        class adminFreeze {};
+	        class adminMarkers {};
 	};
 	
 	class Scripts
